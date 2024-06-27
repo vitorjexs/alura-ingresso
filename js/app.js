22135e32395406444bdacb88c1e797869889215a
Vitor <vitortl@uol.com.br>
2024-06-27 11:56:48 -0300

    Lógica finalizada para tudo

--- js/app.js
+++ js/app.js
@@ -1,32 +1,21 @@
 //variaveis globais
-
-
 let quantidade = document.getElementById("qtd");
 
 let estoquePista = document.getElementById("qtd-pista");
 
-
 let estoqueInferior = document.getElementById("qtd-inferior");
-let estoqueInferiorInt = parseInt(estoqueInferior.textContent, 10);
 
 let estoqueSuperior = document.getElementById("qtd-superior");
-let estoqueSuperiorInt = parseInt(estoqueSuperior.textContent, 10);
-
-
-console.log(estoquePista.textContent);
 
 //função comprar
-
 function comprar() {
 
-    
     let opcoes = document.getElementById("tipo-ingresso");
-    //quando comprar, verificar opção do section
 
     if (opcoes.value == "pista") {
 
         let estoquePistaInt = parseInt(estoquePista.textContent, 10);
-       
+
         if (estoquePistaInt == 0) {
             alert("Ingressos esgotados");
         }
@@ -35,27 +24,55 @@ function comprar() {
             alert("Quantidade em estoque insuficiente");
             estoquePista.textContent;
         } else {
-
-        estoquePista.textContent = estoquePista.textContent - quantidade.value;
-
+            estoquePista.textContent = estoquePista.textContent - quantidade.value;
         }
-        
+
         if (estoquePista < 0) {
             estoquePista.textContent = 0
         }
+
     }
 
-        if (opcoes.value == "inferior") {
-            console.log("WIP")
-        }
-        if (opcoes.value == "superior") {
-            console.log("WIP")
-        }
-        //quando comprar, verificar value do input
+    if (opcoes.value == "inferior") {
+        let estoqueInferiorInt = parseInt(estoqueInferior.textContent, 10);
 
-        console.log(quantidade.value);
+        if (estoqueInferiorInt == 0) {
+            alert("Ingressos esgotados");
+        }
 
-        //diminui numeração de ingressos conforme dados acima
+        if (quantidade.value > estoqueInferiorInt) {
+            alert("Quantidade em estoque insuficiente");
+            estoqueInferior.textContent;
+        } else {
+            estoqueInferior.textContent = estoqueInferior.textContent - quantidade.value;
+        }
+
+        if (estoqueInferior < 0) {
+            estoqueInferior.textContent = 0
+        }
+
+    }
+
+    if (opcoes.value == "superior") {
+        
+        let estoqueSuperiorInt = parseInt(estoqueSuperior.textContent, 10);
+
+        if (estoqueSuperiorInt == 0) {
+            alert("Ingressos esgotados");
+        }
+
+        if (quantidade.value > estoqueSuperiorInt) {
+            alert("Quantidade em estoque insuficiente");
+            estoqueSuperior.textContent;
+        } else {
+            estoqueSuperior.textContent = estoqueSuperior.textContent - quantidade.value;
+        }
+
+        if (estoqueSuperior < 0) {
+            estoqueSuperior.textContent = 0
+        }
+
+    }
 
 }
 
